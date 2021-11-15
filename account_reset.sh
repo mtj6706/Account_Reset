ec3e09c40464b91514ff5ac4bd26e49c87eed0a5
Mark Johnson <mtj6706@rit.edu>
2021-11-15 11:51:51 -0500

    Moved code to git

---
+++ account_reset.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+approved=$(openssl enc -AES-256-CBC -d -in approved_usrs)
+usersA=($(getent passwd | cut -d: -f1))
+delUsers=()
+
+for user in ${usersA[@]} ; do
+	userApproved=$(echo "${approved[@]}" | fgrep --word-regexp "$user")
+	if [ ! "$userApproved" ]; then
+		echo "!!ALERT!!: There is a discrepancy between approved users and current users"
+		echo "	$user is not an approved user, this user will now be killed and removed"
+		delUsers+=($user)
+	fi
+done
+
+for user in ${delUsers[@]}; do
+	if [ ! -d "./${user}_Archive" ]; then
+		mkdir "${user}_Archive"
+	fi
+
+	timestamp=$(date +"%Y-%m-%d:%T")
+	tar -cjv -f "${user}_Archive/${user}_${timestamp}.tar.bz" -C "/home/${user}/"
+
+	killall -u $user
+	crontab -r -u $user
+	# userdel -r $user
+done
